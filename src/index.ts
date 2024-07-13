0b685af0792c686da3b5e41f382f21d1485b3480
w0wayush <ayupandey82@gmail.com>
2024-07-14 02:43:37 +0530

    BE Commit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import referralRouter from "./routes/referral";
 dotenv.config();
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", referralRouter);
 
